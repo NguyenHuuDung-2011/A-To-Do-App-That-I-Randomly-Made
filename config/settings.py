999e4e933225ce87458a6657187f48af28b5fa37
NguyenHuuDung-2011 <huudung5c@gmail.com>
2026-01-03 12:48:09 +0700

    fix : modify settings.py due to static files errors

--- config/settings.py
+++ config/settings.py
@@ -121,4 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR / 'todoapp' / 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / 'todoapp' / 'static',
+]
