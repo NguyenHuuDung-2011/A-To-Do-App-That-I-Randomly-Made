71272de608f71b8ffba7ab4ce2bb143531ab4c7f
NguyenHuuDung-2011 <huudung5c@gmail.com>
2026-01-15 15:11:06 +0700

    fix : set sslmode=require as Render suggested

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,9 @@ DATABASES = {
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
         'PORT': 5432,
-        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
