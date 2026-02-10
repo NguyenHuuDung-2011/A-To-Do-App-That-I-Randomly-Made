66b7a818d9464beff371087b3c7c73434fcc5b02
NguyenHuuDung-2011 <huudung5c@gmail.com>
2026-02-10 14:43:37 +0700

    remove /admin url for security purpose

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,5 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('todoapp.urls')),
 ]
